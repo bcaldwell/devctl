1ac42812cd9b092d478c92890c797a66c7ad6017
Benjamin Caldwell <caldwellbenjamin8@gmail.com>
2017-12-25 14:24:15 -0500

    release v0.0.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ export BUILD_VERSION=`git log -1 --pretty=%B | tr " " "\n" | grep -Ei 'v[0-9]+(\
 
 export BUILDS="linux/amd64 darwin/amd64"
 
-if [[ $BUILD_VERSION ]] 
+if [[ $BUILD_VERSION ]]
 then
   echo "Installing gox and ghr"
   go get github.com/mitchellh/gox
@@ -17,20 +17,20 @@ then
   # Pushing new version and sha to website
   pip install pyyaml --user
   git clone git@github.com:devctl/devctl.github.io.git "$HOME/devctl.github.io"
-  
-  git config --global user.email "caldwellbenjamin8@gmail.com" 
-  git config --global user.name "benjamin-bot" 
+
+  git config --global user.email "caldwellbenjamin8@gmail.com"
+  git config --global user.name "benjamin-bot"
 
   echo "Bundling and generating sha"
   python scripts/update_website_version.py
 
   echo "Pushing to Github"
-  ghr -t $GITHUB_TOKEN -u benjamincaldwell -r devctl $BUILD_VERSION dist/release/
+  ghr -t $GITHUB_TOKEN -u bcaldwell -r devctl $BUILD_VERSION dist/release/
 
 
   cd "$HOME/devctl.github.io"
-  git add . 
-  git commit -m "Version update from circleci" 
+  git add .
+  git commit -m "Version update from circleci"
   git diff
-  git push origin master 
+  git push origin master
 fi
